6fc74457232385275f1d57a4249a985093776580
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-12-21 17:28:44 +0700

    Show text with ssd1306
    Show text with ssd1306

--- example/i2c/text/01-show_text.py
+++ example/i2c/text/01-show_text.py
@@ -7,6 +7,10 @@ from time import sleep
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 oled = SSD1306_I2C(128, 64, i2c)
 
+# Clear Screen
+oled.fill(0)
+oled.show()
+
 # Text Display (Text,x,y,color)
 oled.text("Hello Wolrd!",0,0,1)
 oled.show()
