ddcfb32d61a29f1e4ccff717beeffe573720c6c2
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-12-21 17:26:18 +0700

    Show text with ssd1306
    Show text with ssd1306

---
+++ example/i2c/text/01-show_text.py
@@ -0,0 +1,12 @@
+# Import Library
+from ssd1306 import SSD1306_I2C
+from machine import Pin,SoftI2C
+from time import sleep
+
+# Pin Setup
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+oled = SSD1306_I2C(128, 64, i2c)
+
+# Text Display (Text,x,y,color)
+oled.text("Hello Wolrd!",0,0,1)
+oled.show()
