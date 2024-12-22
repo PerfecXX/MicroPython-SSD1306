a1417800990fb8ab2da3050c048e76db46d772e2
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-12-22 13:42:02 +0700

    Draw Triangle
    Draw Triangle

---
+++ example/i2c/shape/triangle/01-shape_triangle.py
@@ -0,0 +1,17 @@
+# Import Library
+from ssd1306 import SSD1306_I2C
+from machine import Pin, SoftI2C
+
+# Pin Setup
+oled_Pin = SoftI2C(scl=Pin(22), sda=Pin(21))
+
+# OLED Interface
+oled = SSD1306_I2C(128, 64, oled_Pin)
+
+# Clear the display
+oled.fill(0)
+
+# Draw a triangle shape
+#        x1,y1,x2,y2,x3,y3,color,filled?
+oled.triangle(10,10,60,50,110,20,1,0)
+oled.show()
