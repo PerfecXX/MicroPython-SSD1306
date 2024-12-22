4360d739f6d74ef0801f6a00ae0377cfa3a008e6
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-12-22 14:17:52 +0700

    Screen contrast
    Screen contrast

---
+++ example/i2c/screen control/02-screen_contrast.py
@@ -0,0 +1,25 @@
+from machine import Pin, SoftI2C
+from ssd1306 import SSD1306_I2C
+from framebuf import FrameBuffer,MONO_HLSB
+from images import kmitl_logo,cu_logo
+from time import sleep
+
+# Initialize I2C and the OLED display
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21)) 
+oled_width = 128  
+oled_height = 64  
+oled = SSD1306_I2C(oled_width, oled_height, i2c)
+
+while True:
+    # Show test text
+    oled.text("Test Contrast",15,0)
+    
+    # Contrast Level Adjusment
+    for contrast_level in range(0,255):
+        
+        oled.contrast(contrast_level)
+        
+        # Show Contrast Level
+        oled.text("LV:{}".format(contrast_level),50,40,1)
+        oled.show()
+        oled.text("LV:{}".format(contrast_level),50,40,0)
