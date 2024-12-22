5d102d00ba014e981d7ac83945722f74cb4598eb
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-12-22 13:49:18 +0700

    Draw Filled Rectangle
    Draw Filled Rectangle

---
+++ example/i2c/shape/rectangle/02-filled_rectangle.py
@@ -0,0 +1,14 @@
+# Import Library
+from ssd1306 import SSD1306_I2C
+from machine import Pin,SoftI2C
+
+# Pin Setup
+oled_Pin = SoftI2C(scl=Pin(22),sda=Pin(21))
+
+# OLED Interface 
+oled = SSD1306_I2C(128,64,oled_Pin)
+
+# Draw Rect
+# x y width hight color
+oled.rect(3,3,60,60,1,1)
+oled.show()
