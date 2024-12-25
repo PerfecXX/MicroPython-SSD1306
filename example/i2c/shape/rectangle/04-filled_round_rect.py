a9260d83a5dfb0e999f48a481738d2060bda44ac
Teeraphat Kullanankanjana <watit2013@gmail.com>
2024-12-25 14:07:27 +0700

    Draw a filled round rectangle
    Draw a filled round rectangle

---
+++ example/i2c/shape/rectangle/04-filled_round_rect.py
@@ -0,0 +1,13 @@
+# Import Library
+from ssd1306 import SSD1306_I2C
+from machine import Pin, SoftI2C
+
+# Pin Setup
+oled_Pin = SoftI2C(scl=Pin(22), sda=Pin(21))
+
+# OLED Interface
+oled = SSD1306_I2C(128, 64, oled_Pin)
+
+# Draw a rectangle with rounded corners
+oled.round_rect(10, 10, 60, 40, 1, filled=True, radius=10)
+oled.show()
